4d63c686e808dcacad48e4db6190eebe001c9139
BeCrutch <roman.naumenko.94@gmail.com>
2020-11-26 15:05:20 +0300

    dev test

--- app/Http/Controllers/Site/ResourceController.php
+++ app/Http/Controllers/Site/ResourceController.php
@@ -10,9 +10,12 @@ class ResourceController extends Controller
 {
     public function getResource($slug)
     {
+
         $resource = Resource::withoutGlobalScopes()->joinLocalization()->where('slug', $slug)->where('deleted_at', null)->firstOrFail();
+
         $view = Str::lower(class_basename($resource->type));
 
         return view('site.' . $view . '.show', compact('resource'));
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,94 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-Route::group([
-
-    'namespace' => 'Site',
-    'prefix' => LaravelLocalization::setLocale(),
-    #'middleware' => 'trailingSlash'
-
-], function () {
-
-  $currentLocale = LaravelLocalization::getCurrentLocale();
-  $pathInfo = preg_replace('/page=[0-9]+/', '', request()->getPathInfo());
-
-//    dd(config('old_url')[trim($pathInfo, '/')]);
-//    if (isset(config('old_url')[trim($pathInfo, '/')])) {
-//        Route::redirect(config('old_url')[trim($pathInfo, '/')], 301);
-//        redirect(config('old_url')[trim($pathInfo, '/')], 301);
-//    }
-
-//    dd($pathInfo);
-  $url = trim(Str::replaceFirst(('/' . $currentLocale . '/'), '', $pathInfo), '/');
-//    $index = 1;
-//
-//    if ($currentLocale != config('app.fallback_locale')) {
-//        $url = str_replace('/' . $currentLocale, '', $pathInfo);
-//        $index = 2;
-//    }
- // $alias = App\Alias::where('url', $url)->first();
-//    if (!isset($alias)) {
-//        $alias = App\Alias::where('url', request()->segment($index))->first();
-//    }
-
-//    dd($url);
-
-//  if (isset($alias)) {
-//    if ($alias->type == 'brand') {
-//      Route::get('{brandUrl}/{page?}', 'BrandController@show');
-//    }
-//    if ($alias->type == 'category') {
-//      Route::get('{categoryUrl}/{page?}', 'CategoryController@show');
-//    }
-//    if ($alias->type == 'product_group') {
-//      Route::get('{groupUrl}', 'ProductGroupController@show');
-//    }
-//    if ($alias->type == 'product') {
-//      Route::get('{categoryUrl}/{productUrl}/', 'ProductController@show');
-//    }
-//    if ($alias->type == 'page') {
-//      Route::get('/{pageAlias}', 'PageController@index');
-//    }
-//  }
-
-  #Route::get('blog/{aliasOrPage?}/', 'BlogController@index');
-
-  Route::get('/', 'HomeController@index')->name('/');
-  Route::get('/blog', function () {return view('site.blog.index');})->name('blog');
-  #Route::get('sale-points', 'SalePointController@index')->name('sale-points');
-  #Route::post('products/update-prices', 'ProductController@updatePrices');
-  #Route::post('back-call', 'PageController@backСall');
-  #Route::post('order-project', 'PageController@orderProject');
-  #Route::post('order-product', 'PageController@orderProduct');
-
-  #Route::get('calculators', 'CalcController@index');
-
-  #Route::post('calculator', 'PageController@calc');
-  #Route::get('search/{value?}/{page?}', 'SearchController@show');
-  #Route::post('search', 'SearchController@filtering');
-
-  #Route::get('promotions', 'PromotionsController@index');
-  #Route::get('promotions/{value?}', 'PromotionsController@show');
-
-});
-
 Route::prefix(LaravelLocalization::setLocale())->group(function () {
 
     Auth::routes();
-    //Route::get('/home', 'HomeController@index')->middleware(['auth:web', 'checkAccess'])->name('home');
-    Route::get('/home', 'HomeController@index')->name('home');
-
-    #Route::get('/', function () {return view('site.home.index');})->name('/');
-    #Route::get('/category', function () {return view('site.categories.show');})->name('category');
-    #Route::get('/product', function () {return view('site.products.show');})->name('product');
-    #Route::get('/contacts', function () {return view('site.pages.contacts');})->name('contacts');
-    #Route::get('/documents', function () {return view('site.pages.documents');})->name('documents');
-    #Route::get('/documents/certificates', function () {return view('site.pages.certificates');})->name('certificates');
-    #Route::get('/blog', function () {return view('site.blog.index');})->name('blog');
-    #Route::get('/blog/article', function () {return view('site.blog.article');})->name('article');
-
-
 
     Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web']], function () {
         \UniSharp\LaravelFilemanager\Lfm::routes();
@@ -173,6 +88,7 @@ Route::prefix(LaravelLocalization::setLocale())->group(function () {
     });
 
     Route::namespace('Site')->group(function () {
+        Route::get('/', 'HomeController@index')->name('/');
         Route::get('/{slug}', 'ResourceController@getResource')->where('slug', '.*');
     });
 });
