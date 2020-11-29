03a95506fe18bdea23012bd0deac75ae066c911a
maikov121288 <maikov121288@gmail.com>
2020-11-29 20:37:51 +0200

    lidz

--- resources/views/site/category/show.blade.php
+++ resources/views/site/category/show.blade.php
@@ -13,7 +13,8 @@
                             <p>Showing 1–12 of 15 results</p>
                             <div class="form-wrap">
                                 <!--Select 2-->
-                                <select class="form-input select-filter" data-placeholder="Default sorting" data-minimum-results-for-search="Infinity">
+                                <select class="form-input select-filter" data-placeholder="Default sorting"
+                                        data-minimum-results-for-search="Infinity">
                                     <option>Default sorting</option>
                                     <option value="2">Sort by popularity</option>
                                     <option value="3">Sort by average rating</option>
@@ -24,33 +25,11 @@
                             </div>
                         </div>
                         <div class="range range-xs-center range-70">
-                            <div class="cell-sm-6 cell-lg-4">
-                                @include('site.components.product')
-                            </div>
-                            <div class="cell-sm-6 cell-lg-4">
-                                @include('site.components.product')
-                            </div>
-                            <div class="cell-sm-6 cell-lg-4">
-                                @include('site.components.product')
-                            </div>
-                            <div class="cell-sm-6 cell-lg-4">
-                                @include('site.components.product')
-                            </div>
-                            <div class="cell-sm-6 cell-lg-4">
-                                @include('site.components.product')
-                            </div>
-                            <div class="cell-sm-6 cell-lg-4">
-                                @include('site.components.product')
-                            </div>
-                            <div class="cell-sm-6 cell-lg-4">
-                                @include('site.components.product')
-                            </div>
-                            <div class="cell-sm-6 cell-lg-4">
-                                @include('site.components.product')
-                            </div>
-                            <div class="cell-sm-6 cell-lg-4">
-                                @include('site.components.product')
-                            </div>
+                            @foreach($category->products as $product)
+                                <div class="cell-sm-6 cell-lg-4">
+                                    @include('site.components.product')
+                                </div>
+                            @endforeach
                         </div>
                     </div>
                     <!-- Pagination-->
@@ -76,13 +55,15 @@
                             <li>
                                 <div class="form-group form-check custom-checkbox">
                                     <input type="checkbox" class="custom-control-input" id="exampleCheck1">
-                                    <label class="custom-control-label" for="exampleCheck1">Для умывальника <span>(48)</span></label>
+                                    <label class="custom-control-label" for="exampleCheck1">Для умывальника
+                                        <span>(48)</span></label>
                                 </div>
                             </li>
                             <li>
                                 <div class="form-group form-check custom-checkbox">
                                     <input type="checkbox" class="custom-control-input" id="exampleCheck2">
-                                    <label class="custom-control-label" for="exampleCheck2">Для душа <span>(34)</span></label>
+                                    <label class="custom-control-label" for="exampleCheck2">Для душа
+                                        <span>(34)</span></label>
                                 </div>
                             </li>
                             <li>
@@ -94,7 +75,8 @@
                             <li>
                                 <div class="form-group form-check custom-checkbox">
                                     <input type="checkbox" class="custom-control-input" id="exampleCheck4">
-                                    <label class="custom-control-label" for="exampleCheck4">Для биде <span>(15)</span></label>
+                                    <label class="custom-control-label" for="exampleCheck4">Для биде
+                                        <span>(15)</span></label>
                                 </div>
                             </li>
                         </ul>
@@ -107,25 +89,29 @@
                             <li>
                                 <div class="form-group form-check custom-checkbox">
                                     <input type="checkbox" class="custom-control-input" id="exampleCheck5">
-                                    <label class="custom-control-label" for="exampleCheck5">Хромированный <span>(48)</span></label>
+                                    <label class="custom-control-label" for="exampleCheck5">Хромированный
+                                        <span>(48)</span></label>
                                 </div>
                             </li>
                             <li>
                                 <div class="form-group form-check custom-checkbox">
                                     <input type="checkbox" class="custom-control-input" id="exampleCheck6">
-                                    <label class="custom-control-label" for="exampleCheck6">Белый <span>(34)</span></label>
+                                    <label class="custom-control-label" for="exampleCheck6">Белый
+                                        <span>(34)</span></label>
                                 </div>
                             </li>
                             <li>
                                 <div class="form-group form-check custom-checkbox">
                                     <input type="checkbox" class="custom-control-input" id="exampleCheck7">
-                                    <label class="custom-control-label" for="exampleCheck7">Серый <span>(24)</span></label>
+                                    <label class="custom-control-label" for="exampleCheck7">Серый
+                                        <span>(24)</span></label>
                                 </div>
                             </li>
                             <li>
                                 <div class="form-group form-check custom-checkbox">
                                     <input type="checkbox" class="custom-control-input" id="exampleCheck8">
-                                    <label class="custom-control-label" for="exampleCheck8">Антрацит <span>(15)</span></label>
+                                    <label class="custom-control-label" for="exampleCheck8">Антрацит
+                                        <span>(15)</span></label>
                                 </div>
                             </li>
                         </ul>
@@ -136,12 +122,16 @@
                         <h5>Filter By Price</h5>
                         <!--RD Range-->
                         <div class="rd-range-wrap">
-                            <div class="rd-range-inner"><span>Price: </span><span class="rd-range-input-value-1"></span><span>—</span><span class="rd-range-input-value-2"></span></div>
-                            <div class="rd-range" data-min="10" data-max="500" data-start="[75, 244]" data-step="1" data-tooltip="true" data-min-diff="10" data-input=".rd-range-input-value-1" data-input-2=".rd-range-input-value-2"></div>
-                        </div><a class="button button-gray-light-outline" href="#">Filter</a>
+                            <div class="rd-range-inner"><span>Price: </span><span class="rd-range-input-value-1"></span><span>—</span><span
+                                        class="rd-range-input-value-2"></span></div>
+                            <div class="rd-range" data-min="10" data-max="500" data-start="[75, 244]" data-step="1"
+                                 data-tooltip="true" data-min-diff="10" data-input=".rd-range-input-value-1"
+                                 data-input-2=".rd-range-input-value-2"></div>
+                        </div>
+                        <a class="button button-gray-light-outline" href="#">Filter</a>
                     </section>
                 </div>
-                
+
             </div>
         </div>
     </section>
