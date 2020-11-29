bbadf6ddb41358166dea25fa3c6b0c8e953c75aa
Денис Карцев <Kartsev@sandiplus.com>
2020-11-29 21:00:39 +0200

    contacts

--- resources/views/site/components/breadcrumbs.blade.php
+++ resources/views/site/components/breadcrumbs.blade.php
@@ -1,11 +1,17 @@
 <section class="breadcrumbs-custom">
     <div class="shell">
         <div class="breadcrumbs-custom__inner">
-            <p class="breadcrumbs-custom__title">Lorem ipsum dolor</p>
+
+            <p class="breadcrumbs-custom__title">{{ 'Нет заголовка' ?? $someVariable }}</p>
+
             <ul class="breadcrumbs-custom__path">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="#">Page 1</a></li>
-                <li class="active">Page 2</li>
+
+                <li><a href="/">{{ __('Home') }}</a></li>
+
+                <li><a href="#">{{ 'Пусто' ?? $someVariable }}</a></li>
+
+                <li class="active">{{ 'Пусто' ?? $someVariable }}</li>
+
             </ul>
         </div>
     </div>
