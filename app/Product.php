6d64030f3d617a3cdf76a47f83f5a337d55a33c3
Cepairda <cepairda@gmail.com>
2020-12-03 00:20:21 +0200

    Fix: Empty Desc

--- app/Product.php
+++ app/Product.php
@@ -30,7 +30,7 @@ class Product extends Resource
     {
         return $this->attributes['description'] ?? $this->attributes['description'] =
                 $this->getData('description') ??
-                ($description = $this->characteristics->where('name', (LaravelLocalization::getCurrentLocale() == 'ru' ? 'Описание' : 'Опис'))->first()
+                (($description = $this->characteristics->where('name', (LaravelLocalization::getCurrentLocale() == 'ru' ? 'Описание' : 'Опис'))->first())
                     ? $description->value
                     : null);
     }
