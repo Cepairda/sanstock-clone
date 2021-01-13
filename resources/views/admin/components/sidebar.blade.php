471831907ba27530e0972ba320ca85f72554b49b
Alexander Chernozhukov <cepairda@gmail.com>
2021-01-13 15:58:48 +0200

    Icons menu

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -144,6 +144,23 @@
                         </p>
                     </a>
                 </li>
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-cogs"></i>
+                        <p>
+                            Настройки
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('admin.icons.index') }}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Иконки</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </nav>
     </div>
