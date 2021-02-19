90c0c0a9fd9fb27c9d4a17faed4ec2f9e13110e6
Alexander Chernozhukov <cepairda@gmail.com>
2021-02-19 11:58:34 +0200

    Test: no fontawesome

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <!--link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"-->
     <link rel="stylesheet" href="{{ mix('css/site/app.css') }}">
     <style type="text/css">
         {{-- file_get_contents(public_path("/css/site/app.css")) --}}
