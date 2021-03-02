0c0b31b1ebe9c67e5f55fb093376c3911f9f0005
Alexander Chernozhukov <cepairda@gmail.com>
2021-03-02 15:35:34 +0200

    Fix: link on page Documentation

--- resources/views/site/page/certificates.blade.php
+++ resources/views/site/page/certificates.blade.php
@@ -35,39 +35,39 @@
                 <div class="col-12 pt-5">
 
                     <div class="certificatesList">
-                        <a href="https://b2b-sandi.com.ua/files/shares/certificates/lidz-gigienicheskoe-zakljuchenie-1.pdf" target="_blank" class="certificateLink">
+                        <a href="{{ asset('files/shares/certificates/lidz-gigienicheskoe-zakljuchenie-1.pdf') }}" target="_blank" class="certificateLink">
 
-                            <img class="img-data-path" src="https://b2b-sandi.com.ua/photos/shares/certificates/lidz-gigienicheskoe-zakljuchenie-1.png" data-src="https://b2b-sandi.com.ua/photos/shares/certificates/lidz-gigienicheskoe-zakljuchenie-1.png">
+                            <img class="img-data-path" src="{{ asset('photos/shares/certificates/lidz-gigienicheskoe-zakljuchenie-1.webp') }}" data-src="{{ asset('photos/shares/certificates/lidz-gigienicheskoe-zakljuchenie-1.webp') }}">
                             <span class="certificateTitle">LIDZ санитарное заключение</span>
                         </a>
-                        <a href="https://b2b-sandi.com.ua/files/shares/certificates/19_03_2021_Lidz_Sertifikat_-_Smesiteli__Vodonagrevateli_protochnyye.pdf" target="_blank" class="certificateLink">
+                        <a href="{{ asset('files/shares/certificates/19_03_2021_Lidz_Sertifikat_-_Smesiteli__Vodonagrevateli_protochnyye.pdf') }}" target="_blank" class="certificateLink">
 
-                            <img class="img-data-path" src="https://b2b-sandi.com.ua/photos/shares/certificates/19_03_2021_Lidz_Sertifikat_-_Smesiteli__Vodonagrevateli_protochnyye.png" data-src="https://b2b-sandi.com.ua/photos/shares/certificates/19_03_2021_Lidz_Sertifikat_-_Smesiteli__Vodonagrevateli_protochnyye.png">
+                            <img class="img-data-path" src="{{ asset('photos/shares/certificates/19_03_2021_Lidz_Sertifikat_-_Smesiteli__Vodonagrevateli_protochnyye.webp') }}" data-src="{{ asset('photos/shares/certificates/19_03_2021_Lidz_Sertifikat_-_Smesiteli__Vodonagrevateli_protochnyye.webp') }}">
                             <span class="certificateTitle">LIDZ сертификат. Водонагреватели проточные (смесители)</span>
                         </a>
-                        <a href="https://b2b-sandi.com.ua/files/shares/certificates/LIDZ_Zaklyucheniye_gosudarstvennoy_sanitarno-epidemiologicheskoy_ekspertizy.pdf" target="_blank" class="certificateLink">
+                        <a href="{{ asset('files/shares/certificates/LIDZ_Zaklyucheniye_gosudarstvennoy_sanitarno-epidemiologicheskoy_ekspertizy.pdf') }}" target="_blank" class="certificateLink">
 
-                            <img class="img-data-path" src="https://b2b-sandi.com.ua/photos/shares/certificates/LIDZ_Zaklyucheniye_gosudarstvennoy_sanitarno-epidemiologicheskoy_ekspertizy.png" data-src="https://b2b-sandi.com.ua/photos/shares/certificates/LIDZ_Zaklyucheniye_gosudarstvennoy_sanitarno-epidemiologicheskoy_ekspertizy.png">
+                            <img class="img-data-path" src="{{ asset('photos/shares/certificates/LIDZ_Zaklyucheniye_gosudarstvennoy_sanitarno-epidemiologicheskoy_ekspertizy.webp') }}" data-src="{{ asset('photos/shares/certificates/LIDZ_Zaklyucheniye_gosudarstvennoy_sanitarno-epidemiologicheskoy_ekspertizy.webp') }}">
                             <span class="certificateTitle">Lidz Мойки кухонные. Заключение государственной санитарно-эпидемиологической экспертизы</span>
                         </a>
-                        <a href="https://b2b-sandi.com.ua/files/shares/certificates/13_09_2025__Lidz_Dushevyye_kabiny__dushevyye_boksy__gidroboksy__shtory_dlya_vannoy__dveri_dushevyye_steklyannyye__Zaklyucheniye_gosudarstvennoy_sanitarno-epidemiologicheskoy_ekspertizy.pdf" target="_blank" class="certificateLink">
+                        <a href="{{ asset('files/shares/certificates/13_09_2025__Lidz_Dushevyye_kabiny__dushevyye_boksy__gidroboksy__shtory_dlya_vannoy__dveri_dushevyye_steklyannyye__Zaklyucheniye_gosudarstvennoy_sanitarno-epidemiologicheskoy_ekspertizy.pdf') }}" target="_blank" class="certificateLink">
 
-                            <img class="img-data-path" src="https://b2b-sandi.com.ua/photos/shares/certificates/13_09_2025__Lidz_Dushevyye_kabiny__dushevyye_boksy__gidroboksy__shtory_dlya_vannoy__dveri_dushevyye_steklyannyye__Zaklyucheniye_gosudarstvennoy_sanitarno-epidemiologicheskoy_ekspertizy.png" data-src="https://b2b-sandi.com.ua/photos/shares/certificates/13_09_2025__Lidz_Dushevyye_kabiny__dushevyye_boksy__gidroboksy__shtory_dlya_vannoy__dveri_dushevyye_steklyannyye__Zaklyucheniye_gosudarstvennoy_sanitarno-epidemiologicheskoy_ekspertizy.png">
+                            <img class="img-data-path" src="{{ asset('photos/shares/certificates/13_09_2025__Lidz_Dushevyye_kabiny__dushevyye_boksy__gidroboksy__shtory_dlya_vannoy__dveri_dushevyye_steklyannyye__Zaklyucheniye_gosudarstvennoy_sanitarno-epidemiologicheskoy_ekspertizy.webp') }}" data-src="{{ asset('photos/shares/certificates/13_09_2025__Lidz_Dushevyye_kabiny__dushevyye_boksy__gidroboksy__shtory_dlya_vannoy__dveri_dushevyye_steklyannyye__Zaklyucheniye_gosudarstvennoy_sanitarno-epidemiologicheskoy_ekspertizy.webp') }}">
                             <span class="certificateTitle">Lidz Душевые кабины, душевые боксы, гидробоксы, шторы для ванной, двери душевые стеклянные. Заключение государственной санитарно-эпидемиологической экспертизы</span>
                         </a>
-                        <a href="https://b2b-sandi.com.ua/files/shares/certificates/23_09_21_Lidz____________________________________________________________________________.pdf" target="_blank" class="certificateLink">
+                        <a href="{{ asset('files/shares/certificates/23_09_21_Lidz____________________________________________________________________________.pdf') }}" target="_blank" class="certificateLink">
 
-                            <img class="img-data-path" src="https://b2b-sandi.com.ua/photos/shares/certificates/23_09_21_Lidz____________________________________________________________________________.png" data-src="https://b2b-sandi.com.ua/photos/shares/certificates/23_09_21_Lidz____________________________________________________________________________.png">
+                            <img class="img-data-path" src="{{ asset('photos/shares/certificates/23_09_21_Lidz____________________________________________________________________________.webp') }}" data-src="{{ asset('photos/shares/certificates/23_09_21_Lidz____________________________________________________________________________.webp') }}">
                             <span class="certificateTitle">Lidz Сушки для волос (фены) и сушилки для рук. Сертификат и декларация о соответствии</span>
                         </a>
-                        <a href="https://b2b-sandi.com.ua/files/shares/certificates/Vyvod__Aksessuary_dlya_vannoy_komnaty__zerkala__sifony__komplektuyushchiye_i_zapchasti_.pdf" target="_blank" class="certificateLink">
+                        <a href="{{ asset('files/shares/certificates/Vyvod__Aksessuary_dlya_vannoy_komnaty__zerkala__sifony__komplektuyushchiye_i_zapchasti_.pdf') }}" target="_blank" class="certificateLink">
 
-                            <img class="img-data-path" src="https://b2b-sandi.com.ua/photos/shares/certificates/Vyvod__Aksessuary_dlya_vannoy_komnaty__zerkala__sifony__komplektuyushchiye_i_zapchasti_.png" data-src="https://b2b-sandi.com.ua/photos/shares/certificates/Vyvod__Aksessuary_dlya_vannoy_komnaty__zerkala__sifony__komplektuyushchiye_i_zapchasti_.png">
+                            <img class="img-data-path" src="{{ asset('photos/shares/certificates/Vyvod__Aksessuary_dlya_vannoy_komnaty__zerkala__sifony__komplektuyushchiye_i_zapchasti_.webp') }}" data-src="{{ asset('photos/shares/certificates/Vyvod__Aksessuary_dlya_vannoy_komnaty__zerkala__sifony__komplektuyushchiye_i_zapchasti_.webp') }}">
                             <span class="certificateTitle">Lidz Аксессуары для ванной комнаты, зеркала, сифоны, комплектующие и запчасти. Заключение государственной санитарно-эпидемиологической экспертизы.</span>
                         </a>
-                        <a href="https://b2b-sandi.com.ua/files/shares/certificates/04_12_2020_Lidz_Smesiteli_sensornyye__Sertifikat_sootvetstviya_.pdf" target="_blank" class="certificateLink">
+                        <a href="{{ asset('files/shares/certificates/04_12_2020_Lidz_Smesiteli_sensornyye__Sertifikat_sootvetstviya_.pdf') }}" target="_blank" class="certificateLink">
 
-                            <img class="img-data-path" src="https://b2b-sandi.com.ua/photos/shares/certificates/04_12_2020_Lidz_Smesiteli_sensornyye__Sertifikat_sootvetstviya_.png" data-src="https://b2b-sandi.com.ua/photos/shares/certificates/04_12_2020_Lidz_Smesiteli_sensornyye__Sertifikat_sootvetstviya_.png">
+                            <img class="img-data-path" src="{{ asset('photos/shares/certificates/04_12_2020_Lidz_Smesiteli_sensornyye__Sertifikat_sootvetstviya_.webp') }}" data-src="{{ asset('photos/shares/certificates/04_12_2020_Lidz_Smesiteli_sensornyye__Sertifikat_sootvetstviya_.webp') }}">
                             <span class="certificateTitle">Lidz Смесители сенсорные. Сертификат и декларация о соответствии</span>
                         </a>
                     </div>
