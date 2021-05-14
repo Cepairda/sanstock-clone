f1a3d053facdf974a44a395f8463d557cf52099a
Alexander Chernozhukov <cepairda@gmail.com>
2021-05-14 16:32:04 +0300

    Update: defective image

--- resources/views/site/product_group/components/productsTable.blade.php
+++ resources/views/site/product_group/components/productsTable.blade.php
@@ -30,9 +30,14 @@
                 <td>{{ $product["sku"] }}</td>
                 <td>
                     {{--{!! img(['type' => 'product', 'sku' => $product["sku"], 'size' => 70, 'alt' => $product["name"], 'class' => ['lazyload', 'no-src'], 'data-src' => true]) !!}--}}
-                    <img width="150"
+                    @foreach($product->defectiveImages as $key => $value)
+                        <img width="150"
+                             src="/storage/product/{{ $productGroup->sdCode }}/{{ $product->sku }}/{{ $product->sku }}_{{ $key }}.jpg"
+                             alt="">
+                    @endforeach
+                    <!--img width="150"
                          src="{{'https://isw.b2b-sandi.com.ua/imagecache/150x150/' . strval($product["sku"])[0] . '/' . strval($product["sku"])[1] . '/' .  $product["sku"] . '.jpg'}}"
-                         alt="">
+                         alt=""-->
                 </td>
                 <td style="text-align: left;" data-price="{{ $product->price }}" data-oldprice="{{ $product->old_price }}">
                     @if(isset($productsDefectiveAttributes[$product["sku"]]))
