2af53d49a1ad6e05b3d4a447573e47bd33369479
Alexander Chernozhukov <cepairda@gmail.com>
2021-06-09 16:38:58 +0300

    Access by Get Param

--- app/Http/Middleware/CheckIP.php
+++ app/Http/Middleware/CheckIP.php
@@ -3,13 +3,21 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Cookie;
 use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 
 class CheckIP
 {
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        if ($request->ip() == '93.183.206.50' || Str::startsWith($request->ip(), ['172.19', '127.0.0.1'])) {
+        if (
+            $request->ip() == '93.183.206.50'
+            || Str::startsWith($request->ip(), ['172.19', '127.0.0.1'])
+            || $request->get('access') == 'true'
+            || $request->cookie('access')
+        ) {
+            Cookie::queue('access', 'true', 60);
             return $next($request);
         } else {
             return abort(403);
