fe4e52ec94134359ff5306fab56c62c7a5a5777d
serjga <serg862000@gmail.com>
2021-07-06 09:27:16 +0300

    Get response GP

--- resources/views/site/orders/googlePayFrame.blade.php
+++ resources/views/site/orders/googlePayFrame.blade.php
@@ -3,9 +3,10 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Payment</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 </head>
 <body >
-<form id="myForm" >
+<form id="myForm" action="https://secure.platononline.com/post/" method="post">
     <input type="hidden" name="action" value="{{ $data['action'] }}"/>
     <input type="hidden" name="client_key" value="{{ $data['CLIENT_KEY'] }}"/>
     <input type="hidden" name="order_id" value="{{ $data['order_id'] }}"/>
@@ -17,17 +18,36 @@
     <input type="hidden" name="term_url_3ds" value="{{ $data['term_url_3ds'] }}"/>
     <input type="hidden" name="hash" value="{{ $hash }}"/>
 </form>
-<script>
-    let formData = new FormData(document.getElementById("myForm"));
 
-    fetch("https://secure.platononline.com/post/", {
-        method: 'POST',
-        body: formData
-    })
-        .then(r => r.json())
-        .then(function (response) {
+<script>
+
+    $.ajax({
+        url: "https://secure.platononline.com/post/",                                           // url страницы (action_ajax_form.php)
+        type:     "POST",                                                                       // метод отправки
+        dataType: "html",                                                                       // формат данных
+        data: $("#myForm").serialize(),                                                         // Сеарилизуем объект
+        success: function(response) {
+            console.log('Success');                                                             // Данные отправлены успешно
             console.log(response);
-        });
+        },
+        error: function(response) {                                                             // Данные не отправлены
+            console.log('Error');
+            console.log(response);
+        }
+    });
+
+
+
+    // let formData = new FormData(document.getElementById("myForm"));
+    //
+    // fetch("https://secure.platononline.com/post/", {
+    //     method: 'POST',
+    //     body: formData
+    // })
+    //     .then(r => r.json())
+    //     .then(function (response) {
+    //         console.log(response);
+    //     });
 </script>
 </body>
 </html>
